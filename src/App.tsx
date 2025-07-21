5a6a23a801989cceb424de54435f0cd1899c7f42
DESKTOP-I8EMQ2C\Al <al952016@gmail.com>
2025-07-21 15:12:56 +0330

    added Card componnent panel variables.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { Route, Routes } from "react-router";
-import "./App.css";
 import { Home } from "./pages";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
-        {/* <Route element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route> */}
-
       </Routes>
     </>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
-import './Home.scss'
+import "./Home.scss";
+import { Card } from "../../components/Card/Card";
 
 function Home() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Smart Match</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <main className="home">
+      <div className="container">
+        <div className="panel">
+          <Card status="green" />
+        </div>
+
+        <div className="panel">
+          <Card status="orange" />
+        </div>
+
+        <div className="panel">
+          <div className="grid-row data-row">
+            <div className="cell">
+              <div>ID: 1</div>
+            </div>
+
+            <div className="cell">
+              <div>ETD: 11-03-2023</div>
+              <div>ETA: 11-03-2023</div>
+              <div>From: Bandar Abbas</div>
+              <div>To: Istanbul</div>
+            </div>
+
+            <div className="cell">
+              <div>ERD: 10-03-2023</div>
+              <div>Cut-off: 09-03-2023</div>
+              <div>Depot</div>
+              <div>
+                <span className="price">$800</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default Home
+export default Home;
